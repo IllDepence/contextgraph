68a78876a457d687731b270256ca48919293373f
ys8950 <ys8950@aifb-ls3-icarus.aifb.kit.edu>
2022-05-10 08:38:31 +0200

    implement method preprocessing

--- paperswithcode_preprocessing/preprocess_methods.py
+++ paperswithcode_preprocessing/preprocess_methods.py
@@ -1,8 +1,17 @@
+""" From
+        methods.json
+    generate
+        methods.jsonl
+        methods_to_collections.jsonl
+        method_areas.jsonl
+"""
+
 import json
 import os
-from util import url_to_slug
+from util import url_to_pwc_id
 
-base_dir = '/home/ls3data/datasets/paperswithcode/'
+in_dir = '/home/ls3data/datasets/paperswithcode/'
+out_dir = '/home/ls3data/datasets/paperswithcode/preprocessed/'
 
 meths_orig_fn = 'methods.json'
 meths_orig = []
@@ -13,28 +22,60 @@ meths_to_colls = {}
 meth_areas_fn = 'method_areas.jsonl'
 meth_areas = {}
 
-with open(os.path.join(base_dir, meths_orig_fn)) as f:
+with open(os.path.join(in_dir, meths_orig_fn)) as f:
     meths_orig = json.load(f)
 
 for meth in meths_orig:
+    # create preprocessed method object
     meth_new = {
-        key: meth[key]
-        for key in meth
+        key: val
+        for key, val in meth.items()
         if key not in ['collections']  # remove collections
     }
     # add URL slug ID
-    meth_new['id'] = url_to_slug(meth['url'])
-    # build area->collection
+    meth_id = url_to_pwc_id(meth['url'])
+    meth_new['id'] = meth_id
+    # build new methods list
+    meths_new.append(meth_new)
+    # build method->collection
+    #   and area->collection
+    area_prefix = 'pwc:area/'
+    coll_prefix = 'pwc:collection/'
+    meths_to_colls[meth_id] = []
     for coll in meth['collections']:
-        coll_id = coll['collection'].lower().replace(' ', '-')
-        if coll['area-id'] not in meth_areas:
-            meth_areas['area-id'] = {
-                'id': meth_areas['area-id'],
+        coll_id = coll_prefix + coll['collection'].lower().replace(' ', '-')
+        area_id = area_prefix + coll['area_id']
+        meths_to_colls[meth_id].append(coll_id)
+        meths_to_colls
+        if area_id not in meth_areas:
+            meth_areas[area_id] = {
+                'id': area_id,
                 'name': coll['area'],
                 'collections': []
             }
-        meth_areas['area-id']['collections'].append(
+        # insert w/o checks here, remove duplicates later
+        meth_areas[area_id]['collections'].append(
             {'id': coll_id, 'name': coll['collection']}
         )
 
-# TODO: persist result, test
+with open(os.path.join(out_dir, meths_new_fn), 'w') as f:
+    for meth in meths_new:
+        json.dump(meth, f)
+        f.write('\n')
+
+with open(os.path.join(out_dir, meths_to_colls_fn), 'w') as f:
+    for meth, colls in meths_to_colls.items():
+        for coll in colls:
+            json.dump([meth, coll], f)
+            f.write('\n')
+
+with open(os.path.join(out_dir, meth_areas_fn), 'w') as f:
+    for area_id, area in meth_areas.items():
+        # remove duplicates
+        area['collections'] = [
+            dict(tup) for tup in {
+                tuple(coll.items()) for coll in area['collections']
+            }
+        ]
+        json.dump(area, f)
+        f.write('\n')
--- paperswithcode_preprocessing/util.py
+++ paperswithcode_preprocessing/util.py
@@ -2,3 +2,14 @@ def url_to_slug(url):
     url_base = 'https://paperswithcode.com/'
     slug = url[len(url_base):]
     return slug
+
+
+def slug_to_pwc_id(slug):
+    pwc_id_prefix = 'pwc:'
+    return pwc_id_prefix + slug
+
+
+def url_to_pwc_id(url):
+    return slug_to_pwc_id(
+        url_to_slug(url)
+    )
