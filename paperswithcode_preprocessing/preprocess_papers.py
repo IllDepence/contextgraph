38b3c77f87ab0f7d3b898e1ca93e80dd6a26b0da
ys8950 <ys8950@aifb-ls3-icarus.aifb.kit.edu>
2022-05-10 14:01:29 +0200

    implement part of method preprocessing

---
+++ paperswithcode_preprocessing/preprocess_papers.py
@@ -0,0 +1,86 @@
+""" From
+        papers.json
+    generate
+        papers.jsonl
+        tasks_to_papers.jsonl
+        methods_to_tasks.jsonl
+"""
+
+import json
+import os
+from util import url_to_pwc_id
+
+in_dir = '/home/ls3data/datasets/paperswithcode/'
+out_dir = '/home/ls3data/datasets/paperswithcode/preprocessed/'
+
+pprs_orig_fn = 'papers-with-abstracts.json'
+pprs_orig = []
+pprs_new_fn = 'papers.jsonl'
+pprs_new = []
+meths_to_pprs_fn = 'methods_to_papers.jsonl'
+meths_to_pprs = []
+tasks_to_pprs_fn = 'tasks_to_papers.jsonl'
+tasks_to_pprs = []
+meths_orig_fn = 'methods.json'
+meth_name_to_url = dict()
+meth_full_name_to_url = dict()
+
+# build method lookup table
+with open(os.path.join(in_dir, meths_orig_fn)) as f:
+    meths_orig = json.load(f)
+dups = [set(), set()]
+for meth in meths_orig:
+    if meth['full_name'] in meth_full_name_to_url:
+        dups[0].add(meth['full_name'])
+    if meth['name'] in meth_name_to_url:
+        dups[1].add(meth['name'])
+    meth_name_to_url[meth['name']] = meth['url']
+    meth_full_name_to_url[meth['full_name']] = meth['url']
+print(f'{len(dups[0])} duplicate full method names:')
+print(', '.join(dups[0]))
+print(f'{len(dups[1])} duplicate method names:')
+print(', '.join(dups[1]))
+
+with open(os.path.join(in_dir, pprs_orig_fn)) as f:
+    pprs_orig = json.load(f)
+
+invalid_meth_refs = set()
+for ppr in pprs_orig:
+    # create preprocessed dataset object
+    ppr_new = {
+        key: val
+        for key, val in ppr.items()
+        if key not in ['tasks', 'methods']  # remove
+    }
+    # add URL slug ID
+    ppr_id = url_to_pwc_id(ppr['paper_url'])
+    ppr_new['id'] = ppr_id
+    # build new dataset list
+    pprs_new.append(ppr_new)
+    # build methods->papers
+    for meth in ppr['methods']:
+        if meth['full_name'] in meth_full_name_to_url:
+            meth_url = meth_full_name_to_url[meth['full_name']]
+        elif meth['name'] in meth_name_to_url:
+            meth_url = meth_name_to_url[meth['name']]
+        else:
+            invalid_meth_refs.add(meth['name'])
+        meth_id = url_to_pwc_id(meth_url)
+        meths_to_pprs.append(
+            [meth_id, ppr_id]
+        )
+    # build taks->papers
+    # ... here we'll need stuff from eval tbls
+
+print(f'{len(invalid_meth_refs)} invalid method references:')
+print(', '.join(invalid_meth_refs))
+
+with open(os.path.join(out_dir, pprs_new_fn), 'w') as f:
+    for ppr in pprs_new:
+        json.dump(ppr, f)
+        f.write('\n')
+
+with open(os.path.join(out_dir, meths_to_pprs_fn), 'w') as f:
+    for (meth_id, ppr_id) in meths_to_pprs:
+        json.dump([meth_id, ppr_id], f)
+        f.write('\n')
