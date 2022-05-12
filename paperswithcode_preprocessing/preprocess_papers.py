ab67858ce8cec726f5f04688cb263220821b4e43
ys8950 <ys8950@aifb-ls3-icarus.aifb.kit.edu>
2022-05-12 09:57:52 +0200

    extract task to paper edges

--- paperswithcode_preprocessing/preprocess_papers.py
+++ paperswithcode_preprocessing/preprocess_papers.py
@@ -4,12 +4,14 @@
         papers.jsonl
         tasks_to_papers.csv
         methods_to_papers.csv
+    extend
+        tasks.jsonl
 """
 
 import csv
 import json
 import os
-from util import url_to_pwc_id
+from util import url_to_pwc_id, name_to_slug
 
 in_dir = '/home/ls3data/datasets/paperswithcode/'
 out_dir = '/home/ls3data/datasets/paperswithcode/preprocessed/'
@@ -25,6 +27,8 @@ tasks_to_pprs = []
 meths_orig_fn = 'methods.json'
 meth_name_to_url = dict()
 meth_full_name_to_url = dict()
+tasks_preprocessed_fn = 'tasks.jsonl'
+task_name_to_id = dict()
 
 # build method lookup table
 with open(os.path.join(in_dir, meths_orig_fn)) as f:
@@ -37,15 +41,28 @@ for meth in meths_orig:
         dups[1].add(meth['name'])
     meth_name_to_url[meth['name']] = meth['url']
     meth_full_name_to_url[meth['full_name']] = meth['url']
+
+print('- - - - - method reference data - - - - -')
 print(f'{len(dups[0])} duplicate full method names:')
 print(', '.join(dups[0]))
 print(f'{len(dups[1])} duplicate method names:')
 print(', '.join(dups[1]))
 
+# build task lookup dict
+with open(os.path.join(out_dir, tasks_preprocessed_fn)) as f:
+    tasks = [json.loads(line) for line in f]
+    task_name_to_id = {
+        task['name']: task['id']
+        for task in tasks
+    }
+
 with open(os.path.join(in_dir, pprs_orig_fn)) as f:
     pprs_orig = json.load(f)
 
 invalid_meth_refs = set()
+known_task_refs = set()
+tasks_new = dict()
+id_shiftet_tasks = set()
 for ppr in pprs_orig:
     # create preprocessed dataset object
     ppr_new = {
@@ -71,14 +88,60 @@ for ppr in pprs_orig:
             [meth_id, ppr_id]
         )
     # build taks->papers
-    # ... here we'll need stuff from eval tbls
+    # + extend tasks
+    for task_name in ppr['tasks']:
+        if task_name in task_name_to_id:
+            task_id = task_name_to_id[task_name]
+            known_task_refs.add(task_name)
+        else:
+            task_id = 'pwc:task/' + name_to_slug(task_name)
+            try:
+                assert(task_id not in task_name_to_id.values())
+            except AssertionError:
+                # problem example:
+                # Source Code Summarization has slug task/code-summarization
+                # Code Summarization        has slug task/code-summarization-1
+                # -.-
+                id_counter = 1
+                id_appendage = f'-{id_counter}'
+                while task_id in task_name_to_id.values():
+                    task_id = task_id + id_appendage
+                    id_counter += 1
+                    id_appendage = f'-{id_counter}'
+                id_shiftet_tasks.add(task_name)
+            # new task entity
+            tasks_new[task_id] = {
+                'id': task_id,
+                'name': task_name,
+                'description': None,
+                'categories': []
+            }
+        tasks_to_pprs.append([
+            task_id,
+            ppr_id
+        ])
 
+# add new unique tasks to task list
+tasks.extend(tasks_new.values())
+
+print('- - - - - method association - - - - -')
 print(f'{len(invalid_meth_refs)} invalid method references:')
 print(', '.join(invalid_meth_refs))
+
+print('- - - - - task association - - - - -')
+print(f'{len(known_task_refs)} known task references')
+print(f'{len(tasks_new)} newly created tasks')
+print(f'{len(id_shiftet_tasks)} task with shifted ID:')
+print(', '.join(id_shiftet_tasks))
 
 with open(os.path.join(out_dir, pprs_new_fn), 'w') as f:
     for ppr in pprs_new:
         json.dump(ppr, f)
+        f.write('\n')
+
+with open(os.path.join(out_dir, tasks_preprocessed_fn), 'w') as f:
+    for task in tasks:
+        json.dump(task, f)
         f.write('\n')
 
 with open(os.path.join(out_dir, meths_to_pprs_fn), 'w') as f:
@@ -93,3 +156,16 @@ with open(os.path.join(out_dir, meths_to_pprs_fn), 'w') as f:
     ])
     for (meth_id, ppr_id) in meths_to_pprs:
         csv_writer.writerow([meth_id, ppr_id])
+
+with open(os.path.join(out_dir, tasks_to_pprs_fn), 'w') as f:
+    csv_writer = csv.writer(
+        f,
+        delimiter=',',
+        quoting=csv.QUOTE_NONE
+    )
+    csv_writer.writerow([
+        'task_id',
+        'paper_id'
+    ])
+    for (task_id, ppr_id) in tasks_to_pprs:
+        csv_writer.writerow([task_id, ppr_id])
