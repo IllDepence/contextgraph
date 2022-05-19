403e60e9be57de085476043f65aff926e43a432b
Tarek Saier <tareksaier@gmail.com>
2022-05-19 21:15:49 +0200

    add method collections and areas to graph

--- contextgraph/util/graph.py
+++ contextgraph/util/graph.py
@@ -2,61 +2,75 @@ import csv
 import os
 import json
 import networkx as nx
-from contextgraph import config
+from contextgraph import config as cg_config
 
 
-def _load_node_tuples(data_dir):
+def _load_node_tuples():
     """ loads nodes as (<id>, <properties>) tuples
     """
 
-    pprs_fn = 'papers.jsonl'
-    meths_fn = 'methods.jsonl'
-    dsets_fn = 'datasets.jsonl'
-    tasks_fn = 'tasks.jsonl'
-    modls_fn = 'models.jsonl'
     entity_tuples = []
-    for fn in [pprs_fn, meths_fn, dsets_fn, tasks_fn, modls_fn]:
-        with open(os.path.join(data_dir, fn)) as f:
+    # first all regularly stored entities
+    for fn in [
+        cg_config.graph_pprs_fn,
+        cg_config.graph_meths_fn,
+        cg_config.graph_dsets_fn,
+        cg_config.graph_tasks_fn,
+        cg_config.graph_modls_fn
+    ]:
+        with open(os.path.join(cg_config.graph_data_dir, fn)) as f:
             for line in f:
                 entity = json.loads(line)
                 entity_tuples.append((entity['id'], entity))
+    # then some special processing for the areas to collections data
+    with open(os.path.join(
+        cg_config.graph_data_dir,
+        cg_config.graph_meth_areas_fn
+    )) as f:
+        for line in f:
+            # area
+            area = json.loads(line)
+            entity_tuples.append((
+                area['id'],
+                {
+                    'id': area['id'],
+                    'name': area['name']
+                }
+            ))
+            # all contained collections
+            for coll in area['collections']:
+                entity_tuples.append(
+                    (coll['id'], coll)
+                )
     return entity_tuples
 
 
-def _load_edge_tuples(data_dir):
+def _load_edge_tuples():
     """ loads edges as (<id>, <properties>) tuples
     """
 
-    # used_in_paper
-    meths_to_pprs_fn = 'methods_to_papers.csv'
-    dsets_to_pprs_fn = 'datasets_to_papers.csv'
-    tasks_to_pprs_fn = 'tasks_to_papers.csv'
-    modls_to_pprs_fn = 'models_to_papers.csv'
-    # used_together (tread as symmetrical?)
-    meths_to_dsets_fn = 'methods_to_datasets.csv'
-    dsets_to_tasks_fn = 'datasets_to_tasks.csv'
-    # part_of
-    meths_to_colls_fn = 'methods_to_collections.csv'
-    tasks_to_subtasks_fn = 'tasks_to_subtasks.csv'
-    # cites
-    pprs_to_pprs_fn = 'papers_to_papers.csv'
+    # first all regularly stored edges
     edge_types = [
-        [meths_to_pprs_fn, 'used_in_paper'],
-        [dsets_to_pprs_fn, 'used_in_paper'],
-        [tasks_to_pprs_fn, 'used_in_paper'],
-        [modls_to_pprs_fn, 'used_in_paper'],
-        [meths_to_dsets_fn, 'used_together'],
-        [dsets_to_tasks_fn, 'used_together'],
-        [meths_to_colls_fn, 'part_of'],
-        [tasks_to_subtasks_fn, 'part_of'],
-        [pprs_to_pprs_fn, 'cites']
+        # used_in_paper
+        [cg_config.graph_meths_to_pprs_fn, 'used_in_paper'],
+        [cg_config.graph_dsets_to_pprs_fn, 'used_in_paper'],
+        [cg_config.graph_tasks_to_pprs_fn, 'used_in_paper'],
+        [cg_config.graph_modls_to_pprs_fn, 'used_in_paper'],
+        # used_together (tread as symmetrical?)
+        [cg_config.graph_meths_to_dsets_fn, 'used_together'],
+        [cg_config.graph_dsets_to_tasks_fn, 'used_together'],
+        # part_of
+        [cg_config.graph_meths_to_colls_fn, 'part_of'],
+        [cg_config.graph_tasks_to_subtasks_fn, 'part_of'],
+        # cites
+        [cg_config.graph_ppr_to_ppr_fn, 'cites']
     ]
-    edges = []
+    edge_tuples = []
     for (fn, edge_type) in edge_types:
         header_idxs = [0, 1]
         if edge_type == 'cites':
             header_idxs = [3, 4]
-        with open(os.path.join(data_dir, fn)) as f:
+        with open(os.path.join(cg_config.graph_data_dir, fn)) as f:
             csv_reader = csv.DictReader(
                 f,
                 delimiter=',',
@@ -66,20 +80,36 @@ def _load_edge_tuples(data_dir):
             for row in csv_reader:
                 tail_id = row[headers[header_idxs[0]]]
                 head_id = row[headers[header_idxs[1]]]
-                edges.append(
+                edge_tuples.append(
                     (
                         tail_id,
                         head_id,
                         {'type': edge_type}
                     )
                 )
-    return edges
+    # then some special processing for the areas to collections data
+    with open(os.path.join(
+        cg_config.graph_data_dir,
+        cg_config.graph_meth_areas_fn
+    )) as f:
+        for line in f:
+            area = json.loads(line)
+            head_id = area['id']
+            for coll in area['collections']:
+                tail_id = coll['id']
+                edge_tuples.append(
+                    (
+                        tail_id,
+                        head_id,
+                        {'type': 'part_of'}
+                    )
+                )
+    return edge_tuples
 
 
 def load_graph():
-    data_dir = config.graph_data_dir
-    node_tuples = _load_node_tuples(data_dir)
-    edge_tuples = _load_edge_tuples(data_dir)
+    node_tuples = _load_node_tuples()
+    edge_tuples = _load_edge_tuples()
     G = nx.DiGraph()
     G.add_nodes_from(node_tuples)
     G.add_edges_from(edge_tuples)
