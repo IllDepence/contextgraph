a41e70571417303c096a76456d90cfbeaa044070
ujvxd <ujvxdQstudent.kit.edu>
2022-06-19 19:48:42 +0200

    add package for machine learning
    - parameter.py contains necessaty hyperparameters for constructing and fitting Node2Vec model
    - node2vec_embedder.py contains one class that has functions of embedding all samples into vectors and outputs one dataframe
    - link_prediction.py is the main file which calls above two
    - next step would be to write Boiler-Plate-Code for ML classification

---
+++ graphml/link_prediction.py
@@ -0,0 +1,10 @@
+from contextgraph import config as cg_config
+from parameters import Parameters
+from node2vec_embedder import Node2VecEmbedder
+
+param = Parameters()
+node2vec_ = Node2VecEmbedder(param_object=param, pattern="avg")
+embeddings = node2vec_.node_embedding(directory=cg_config.graph_samples,
+                                      directed=True  # todo: what kind of graph to use?
+                                     )
+embeddings.to_pickle("/local/users/ujvxd/env/test.pkl")
---
+++ graphml/node2vec_embedder.py
@@ -0,0 +1,148 @@
+import networkx as nx
+from node2vec import Node2Vec
+import os
+import json
+import pandas as pd
+import numpy as np
+import warnings
+from tqdm import tqdm
+
+
+class Node2VecEmbedder():
+
+    def __init__(self, param_object, pattern="avg"):
+
+        '''
+        param_object: a object from dataclass that stores all necessary hyperparameter for Node2Vec
+        pattern: the pattern of how the final embedding of two embeddings of targets nodes are calculated
+
+        '''
+        self.param = param_object
+        self.pattern = pattern
+        self.embeddings = None
+
+
+    def prepare_samples(self):
+
+        file_pairs = []
+        numbers = ["0" + str(i) for i in range(10)] + [str(i) for i in range(10, self.param.NUM_SAMPLES)]
+
+        for num in numbers:
+            file_graph = "pair_graph_sample_" + num + "_graph.json"
+            file_pred = "pair_graph_sample_" + num + "_prediction_edge.json"
+            file_pairs.append((file_graph, file_pred))
+        return file_pairs
+
+
+    def generate_atom_graph(self, file_dir, file_graph, file_pred, directed=True, export=False):
+
+        file_graph_path = os.path.join(file_dir, file_graph)
+        file_pred_path = os.path.join(file_dir, file_pred)
+        with open(file_graph_path, "r") as g:
+            data = json.load(g)
+        with open(file_pred_path, "r") as p:
+            predicting = json.load(p)
+        node_pair_to_predict = predicting["edge"]
+        elements = data.get("elements", {})
+
+        nodes = []
+        pairs = []
+        for node in elements["nodes"]:
+            info = node["data"]
+            nodes.append((info["id"],
+                          {"type": info["type"]}))
+        for edge in elements["edges"]:
+            structure = edge["data"]
+            pairs.append((structure["source"],
+                          structure["target"],
+                          {"type": structure["type"]}))
+        if directed:
+            atom_graph = nx.DiGraph()
+        else:
+            atom_graph = nx.Graph()
+        atom_graph.add_nodes_from(nodes)
+        atom_graph.add_edges_from(pairs)
+
+        if export:
+            final_name = self.process_name(node_pair_to_predict)
+            # cooc_pprs = predicting["cooc_pprs"]
+            nx.write_graphml(atom_graph, final_name)
+
+        return atom_graph, node_pair_to_predict
+
+    @staticmethod
+    def process_name(node_pair_to_predict):
+        '''simply get rid of "pwc" and return the concatenation of two node names'''
+        node_names = [node.split(":")[-1].replace("/", "_") \
+                      for node in node_pair_to_predict]
+        final_name = node_names[0] + "-" + node_names[1]
+        return final_name
+
+    @staticmethod
+    def operate(df_2r, pattern):
+
+        if pattern == "avg":
+            return df_2r.sum()
+        elif pattern == "hadamard":
+            return df_2r.iloc[0, :] * df_2r.iloc[1, :0]
+        elif pattern == "l1":
+            return np.abs(df_2r.iloc[0, :] - df_2r.iloc[1, :])
+        elif pattern == "l2":
+            return (df_2r.iloc[0, :] - df_2r.iloc[1, :]) ** 2
+        else:
+            message = 'A unsupported pattern for processing embeddings of \
+                        two target nodes has been given, please use a correct \
+                        pattern from either "avg", "hadamard", "l1" or "l2"'
+            warnings.warn(message)
+
+    def node_embedding(self, directory, directed=True, export_each_graph=False):
+
+        file_pairs = self.prepare_samples()
+        df_emb = pd.DataFrame(columns=range(self.param.DIMENSIONS))
+
+        for file_pair in tqdm(file_pairs, desc="processing file pairs:"):
+            file_graph = file_pair[0]
+            file_pred = file_pair[1]
+            graph, node_pair_to_predict = self.generate_atom_graph(file_dir=directory,
+                                                                   file_graph=file_graph,
+                                                                   file_pred=file_pred,
+                                                                   directed=directed,
+                                                                   export=export_each_graph)
+
+            node2vec = Node2Vec(graph,
+                                dimensions=self.param.DIMENSIONS,
+                                walk_length=self.param.WALK_LENGTH,
+                                num_walks=self.param.NUM_WALKS,
+                                seed=self.param.SEED
+                                )
+
+            model = node2vec.fit(
+                window=self.param.WINDOW,
+                min_count=self.param.MIN_COUNT,
+                batch_words=self.param.BATCH_WORDS
+            )
+
+            embeddings = pd.DataFrame(
+                [model.wv.get_vector(str(n)) for n in graph.nodes()],
+                index=graph.nodes
+            )
+
+            try:
+                emb_target_nodes = embeddings.loc[node_pair_to_predict, :]
+                emb_serie = self.operate(emb_target_nodes, self.pattern)
+            except:
+                emb_serie = pd.Series(index=range(128), dtype=np.float64)
+
+            final_name = self.process_name(node_pair_to_predict)
+            df_emb.loc[final_name, :] = emb_serie
+
+        if df_emb.isnull().sum().sum() > 0:
+            message = "targets node in prediction files are not consistent \
+                        with nodes in graph file, check dataframe manully to \
+                        get more information"
+            warnings.warn(message)
+
+        self.embeddings = df_emb
+
+        return df_emb
+
