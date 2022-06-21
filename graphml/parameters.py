e1fdbb4eca34b344dc65b44f5b4a7fb317ef5c0a
ujvxd <ujvxdQstudent.kit.edu>
2022-06-21 11:25:12 +0200

    adapt the name of hyperparam

--- graphml/parameters.py
+++ graphml/parameters.py
@@ -4,17 +4,17 @@ from dataclasses import dataclass
 class Parameters():
     """Class for reserving the hyperparameters."""
     # todo: consider to set hyperparam according to the graph characteristics (num of nodes etc.)
-    WINDOW: int = 6
+    WINDOW: int = 10
     MIN_COUNT: int = 1
-    BATCH_WORDS: int = 4
+    BATCH_WORDS: int = 5
 
     DIMENSIONS: int = 128
-    WALK_LENGTH: int = 30
-    NUM_WALKS: int = 200
+    WALK_LENGTH: int = 20
+    NUM_WALKS: int = 20
     SEED: int = 42
 
     # number of target node pairs in the directory
-    NUM_SAMPLES: int = 1000
+    NUM_SAMPLES_PER_LABEL: int = 500
 
     # directory
     # DIR : str = "/tmp/sc_graph_samples"
