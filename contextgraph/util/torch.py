1f1a5f4831e9a84c45b6134a13365663af3ef02e
ys8950 <ys8950@aifb-ls3-icarus.aifb.kit.edu>
2023-03-06 16:03:16 +0100

    implement description generation for model nodes

--- contextgraph/util/torch.py
+++ contextgraph/util/torch.py
@@ -14,18 +14,52 @@ def load_full_graph():
     raise NotImplementedError
 
 
+def get_artifact_description(node_attrs, G):
+    """ Return a string that is used as the description of the given
+        artifact.
+    """
+
+    if node_attrs['type'] == 'model':
+        # dont’t have a description in and of themselves, so we create one from
+        # - the model’s name
+        # - the titles of the papers that use the model
+        # - the titles of the tasks the is was used for
+        task_names = set()
+        for (task_id, dset) in node_attrs['evaluations']:
+            task = G.nodes.get(task_id)
+            if task is not None:
+                task_names.add(task['name'])
+        tasks_insert = ' and '.join(
+            task_names
+        )
+        pprs_insert = ' and '.join(
+            node_attrs['using_paper_titles']
+        )
+        descr = (
+            f'The model {node_attrs["name"]} '
+            f'is used for the tasks {tasks_insert}. '
+            f'It has been used in in the papers {pprs_insert}.'
+        )
+    else:
+        # datasets, methods and tasks already have descriptions
+        descr = node_attrs['description']
+
+    return descr
+
+
 def load_entity_combi_graph():
     """ Return entity combi graph in a form usable with torch geometric.
     """
 
     # load node and edge tuples (with full, non-numeric features)
     node_tuples = _load_node_tuples(entities_only=True)
+    # combi_edge: transform paper nodes into relationship between artifacts
+    # (method, model, dataset, task)
     edge_tuples = _load_entity_combi_edge_tuples(
         final_node_set=set([ntup[0] for ntup in node_tuples]),
         scheme='weight'  # use weight scheme b/c it gives us a single integer
         #                  feature for edges rather than a variable length list
     )
-    # combi_edge: extend paper nodes into relationship between (paper, artifact, method, dataset)
     # build networkx graph for later conversion
     G = nx.Graph()
     # convert node features
@@ -73,4 +107,3 @@ def load_entity_combi_graph():
         group_edge_attrs=['weight']
     )
     return data
-
